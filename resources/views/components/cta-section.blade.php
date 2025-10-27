d72e692f132d6ab94a304870d9ed464c68003dc1
SherlyMaycana <55482287+L200190211@users.noreply.github.com>
2025-10-27 16:38:55 +0700

    update cta

--- resources/views/components/cta-section.blade.php
+++ resources/views/components/cta-section.blade.php
@@ -1,6 +1,5 @@
 <section class="cta-section {{ $stylesection }} text-center">
-    <h3 class="text-4xl font-bold {{ $titleColor }} mb-12">{{ $title }}
-    </h3>
+    <h3 class="text-4xl font-bold {{ $titleColor }} mb-12">{{ $title }}</h3>
     <div class="flex space-x-4 justify-center mt-8">
         <a href="{{ $btnUrl }}"
             class="bg-[#7DB8D8] hover:bg-[#6ca7c8] text-white px-6 py-3 font-semibold transition">
